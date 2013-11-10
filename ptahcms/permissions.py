2fbb0d18b7150ad7f9b780640f0489e5502acd3e
Marco Strobel <mail@strobel.ch>
2013-11-10 14:55:01 +0100

    fix typo

--- ptahcms/permissions.py
+++ ptahcms/permissions.py
@@ -2,7 +2,7 @@ import ptah
 
 View = ptah.Permission('ptahcms:ViewContent', 'View content')
 AddContent = ptah.Permission('ptahcms:AddContent', 'Add content')
-ModifyContent = ptah.Permission('ptahcms:EditContent', 'Modify content')
+ModifyContent = ptah.Permission('ptahcms:ModifyContent', 'Modify content')
 RenameContent = ptah.Permission('ptahcms:RenameContent', 'Rename content')
 DeleteContent = ptah.Permission('ptahcms:DeleteContent', 'Delete content')
 ShareContent = ptah.Permission('ptahcms:ShareContent', 'Share content')
