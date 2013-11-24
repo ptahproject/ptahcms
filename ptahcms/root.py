4bb41d6c63cc091fac8f5679b05d0b6f719285d7
Marco Strobel <mail@strobel.ch>
2013-11-25 00:34:04 +0100

    undo

--- ptahcms/root.py
+++ ptahcms/root.py
@@ -102,7 +102,6 @@ class ApplicationFactory(object):
             Session = ptah.get_session()
             Session.add(root)
             Session.flush()
-            Session.close()
 
         root.__root_path__ = self.path
         policy = self.policy(request)
