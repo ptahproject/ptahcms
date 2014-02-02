e20e7d495fecdc7c04553de12b2e3f1ae9fb2168
Marco Strobel <mail@strobel.ch>
2014-02-02 02:22:55 +0100

    remove site title setting

--- ptahcms/settings.py
+++ ptahcms/settings.py
@@ -6,15 +6,3 @@ _ = translationstring.TranslationStringFactory('ptahcms')
 
 CFG_ID_CMS = 'ptahcms'
 
-
-ptah.register_settings(
-    CFG_ID_CMS,
-
-    form.TextField(
-        name = 'brand-name',
-        title = _('Brand name'),
-        description = _('Name of your site.'),
-        default = 'Ptah CMS'),
-
-    title = 'Ptah cms settings',
-    )
